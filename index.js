bcae5013bccd497876f10f550543a9bfa116670f
sarthak <sarthakjaiswal45@gmail.com>
2022-03-17 13:34:24 +0530

    deploy

--- index.js
+++ index.js
@@ -71,5 +71,5 @@ app.post('/post_case_intake', (req, res) => {
   console.log(sql);
 });
 
-const port = 5500;
+const port = 6000;
 app.listen(port, () => console.info(`[Server] > Listening on port ${port}`));
